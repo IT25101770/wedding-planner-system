67d5610e9c5c37878b3685e7330017b06b06c01a
IT25101770 <it25101770@my.sliit.lk>
2026-05-16 12:33:27 +0530

    ADD Structure

---
+++ src/main/java/com/weddingplanner/WeddingPlannerApplication.java
@@ -0,0 +1,15 @@
+package com.weddingplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingPlannerApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(WeddingPlannerApplication.class, args);
+        System.out.println("http://localhost:8080/");
+
+    }
+
+}
