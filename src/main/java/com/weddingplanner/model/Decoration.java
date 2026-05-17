89f27e3d8b768100ea2237240d720d06b8916422
IT25101951 <it25101951@my.sliit.lk>
2026-05-17 10:14:32 +0530

    add Decoration class

--- src/main/java/com/weddingplanner/model/Decoration.java
+++ src/main/java/com/weddingplanner/model/Decoration.java
@@ -1,1 +1,16 @@
-//Praveen
+package com.weddingplanner.model;
+
+public class Decoration extends Vendor {
+    public Decoration() {
+        setCategory("Decoration");
+    }
+
+    public Decoration(String id, String name, String phone, String location, double packagePrice, boolean available) {
+        super(id, name, "Decoration", phone, location, packagePrice, available);
+    }
+
+    @Override
+    public String getPackageDescription() {
+        return "Stage, floral, table and theme decoration";
+    }
+}
